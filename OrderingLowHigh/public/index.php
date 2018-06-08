7fbfb044a1d3365f7e39f9c4b981380f863cd5fb
Julio Cesar <vcjuliocesar@outlook.com>
2018-06-07 20:40:44 -0500

    refactoring code

--- OrderingLowHigh/public/index.php
+++ OrderingLowHigh/public/index.php
@@ -1,10 +1,12 @@
 <?php
 
+use app\OrderingLowHigh;
+
 require '../vendor/autoload.php';
 
 $arreglo = [3,1,2,5,8,9,6,3,1,4,5,78,55,0,200];
 
-$Ordering= new app\OrderingLowHigh($arreglo);
+$Ordering = new OrderingLowHigh($arreglo);
 
 $Ordering->Ordering();
 
